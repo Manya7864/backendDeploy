7c6aaf116e974a1f8f50e401bcb45908b52d4f71
Manya7864 <manishyadav78642080@gmail.com>
2022-12-04 13:18:21 +0530

    backend2

--- Model/User.Model.js
+++ Model/User.Model.js
@@ -2,6 +2,8 @@ const mongoose=require("mongoose")
 const userSchema=new mongoose.Schema({
     email:{type:String,required:true},
     password:{type:String,required:true},
+    count:{type:Number},
+    loginDate:{type:Date},
 })
 const userModel=mongoose.model("user",userSchema)
-module.exports=userModel
+module.exports=userModel;
--- Routes/User.Route.js
+++ Routes/User.Route.js
@@ -6,53 +6,131 @@ const express = require("express");
 const userrouter = express.Router();
 
 userrouter.post("/signup", async (req, res) => {
-  const {email, password } = req.body;
- await bcrypt.hash(password, 6, async function (err, hash) {
+  const { email, password } = req.body;
+  await bcrypt.hash(password, 6, async function (err, hash) {
     if (err) {
       return res.send("Signup fail");
     }
     const user = new userModel({
       email,
       password: hash,
+      count: 0,
+      loginDate: new Date(),
     });
     await user.save();
     res.send("signup succesfully");
   });
 });
 
-
-
 userrouter.post("/login", async (req, res) => {
-    const { email, password } = req.body;
-    const user = await userModel.findOne({ email });
-    if (!user) {
-      return res.send("USER not found");
-    }
-    const hashpwd = user.password;
-    bcrypt.compare(password, hashpwd, function (err, result) {
+  const { email, password } = req.body;
+  const user = await userModel.findOne({ email });
+
+  if (!user) {
+    return res.send("USER not found");
+  }
+  let currentDate = new Date();
+  console.log("TIME VLIDATE", Date.parse(currentDate),Date.parse(user.loginDate));
+
+  const hashpwd = user.password;
+
+  if (user.count < 5) {
+    bcrypt.compare(password, hashpwd, async function (err, result) {
       if (err) {
         return res.send("try again");
       }
+
       if (result) {
-        const token = jwt.sign(
-          { email: user.email},
-          process.env.JWT_KEY
+        let newLoginData = {
+          email: email,
+          password: hashpwd,
+          count: 0,
+          loginDate: new Date(),
+        };
+
+        let newData = await userModel.findOneAndUpdate(
+          { email: email },
+          newLoginData,
+          { new: true }
         );
+
+        const token = jwt.sign({ email: user.email }, process.env.JWT_KEY);
+
         return res.send({
           message: "login succesfull",
           token: token,
-          
+          userEmail: user.email,
+          result: newData,
         });
-      } else {
-        res.send("invalid credential");
+      } 
+      else 
+      {
+        let newCount = user.count + 1;
+        let limitDate = new Date(new Date().getTime() + 60*60*24*1000);
+        console.log("COunt ", newCount);
+        let newLoginData = {
+          email: email,
+          password: result.password,
+          count: newCount,
+          loginDate: limitDate,
+        };
+        let newData = await userModel.findOneAndUpdate(
+          { email: email },
+          newLoginData,
+          { new: true }
+        );
+        res.send({ message: "invalid credential", result: newData });
       }
     });
-  });
+  } 
+  else 
+  {
+    let currentDate = new Date();
+    if (Date.parse(currentDate)>= Date.parse(user.loginDate)) {
+      console.log("TIMER STOP 60SEC Completed");
+      bcrypt.compare(password, hashpwd, async function (err, result) {
+        if (err) {
+          return res.send("try again");
+        }
 
-  userrouter.post("/logout", async (req, res) => {
-    const token =req.body.token
-    const user = await userModel.findOne({ email });
-    res.send({ email: user.email });
-  });
+        console.log(result)
 
-  module.exports = userrouter;
+        if (result) {
+          let newLoginData = {
+            email: email,
+            password: hashpwd,
+            count: 0,
+            loginDate: new Date(),
+          };
+
+          let newData = await userModel.findOneAndUpdate(
+            { email: email },
+            newLoginData,
+            { new: true }
+          );
+          const token = jwt.sign({ email: user.email }, process.env.JWT_KEY);
+
+          res.send({
+            message: "login succesfull",
+            token: token,
+            userEmail: user.email,
+            result: newData,
+          });
+        }
+        else{
+          res.send("Invalid Credential")
+        }
+      });
+    }else{
+    res.send({ message: "Limit reached", user: user });
+    }
+  }
+});
+
+userrouter.post("/logout", async (req, res) => {
+  const token = req.body.token;
+  const user = await userModel.findOne({ email });
+  res.send({ email: user.email });
+});
+
+module.exports = userrouter;
